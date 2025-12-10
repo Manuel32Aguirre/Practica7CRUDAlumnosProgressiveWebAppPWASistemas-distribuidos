699566204c3a78293225215fefae0011b661b8eb
vic-s <programadorjrsemad@gmail.com>
2025-12-10 17:14:27 -0600

    Arreglo en server.js

--- server.js
+++ server.js
@@ -6,7 +6,11 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-const PORT = 3000;
+
+// --- CAMBIO IMPORTANTE ---
+// Azure te asigna un puerto dinámico. Debes usar process.env.PORT.
+// Si no existe (como en tu PC), usa el 3000.
+const PORT = process.env.PORT || 3000;
 
 // Servir archivos estáticos (html, js, css, icons, manifest, etc.)
 app.use(express.static(__dirname));
@@ -18,5 +22,5 @@ app.get("/", (req, res) => {
 
 // Iniciar servidor
 app.listen(PORT, () => {
-  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+  console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
